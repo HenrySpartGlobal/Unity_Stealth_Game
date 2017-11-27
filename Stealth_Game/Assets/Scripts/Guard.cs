6f6b1d809a32ca75d227ccf29eab6bdef05d8d4f
Henry <henry_koleoso@hotmail.co.uk>
2017-11-27 15:44:22 +0000

    Started working on guard

---
+++ Stealth_Game/Assets/Scripts/Guard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour {
+
+	public Transform pathHolder;
+	//Looping through the path Game object, adding a texture/Gizmo to each Child object so it is visible in the Scene
+	void OnDrawGizmos() {
+		foreach (Transform waypoint in pathHolder) {
+			Gizmos.DrawSphere (waypoint.position, .3f);
+
+		}
+
+	}
+
+}
